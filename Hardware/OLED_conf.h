87eb0054b1951cd49174bc5c7c82c236b11b970b
ori71e2 <ori71e2@gmail.com>
2022-12-29 22:43:08 +0800

    add

---
+++ Hardware/OLED_conf.h
@@ -0,0 +1,7 @@
+#ifndef __OLED_CONF_H
+#define __OLED_CONF_H
+
+#define OLED_W_SCL_GPIO_PIN   GPIO_Pin_14
+#define OLED_W_SDA_GPIO_PIN   GPIO_Pin_15
+
+#endif
--- User/main.c
+++ User/main.c
@@ -6,9 +6,19 @@
 #include "mike.h"
 #include "test.h"
 #include "fft.h"
-
+#include "OLED.h"
 int main() {
   led_Init();
+	mike_Init();
+	OLED_Init();
+	OLED_ShowNum(1, 1, ADC_SourceData[0], 5);
+	// OLED_ShowChar(1, 1, 'A');
+	// OLED_ShowString(1, 3, "HelloWorld!");
+	// OLED_ShowNum(2, 1, 12345, 5);
+	// OLED_ShowSignedNum(2, 7, -66, 2);
+	// OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	// OLED_ShowBinNum(4, 1, 0xAA55, 16);
+	
   while (1)
 	{
 		// arm_sin_cos_example_f32();
